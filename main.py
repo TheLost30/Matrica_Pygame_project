6c8a5b4008d58e966e30b2c5906fb181547b110a
the_lost30 <gribkirill44@gmail.com>
2023-02-18 17:24:22 +0500

    l

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import pygame
 
 from images import *
+import json
 
 
 class Map(SpriteGroup):
 	def __init__(self, map_width, map_height):
 		super().__init__()
+		self.top_block_image = load_image(r'Map/Dirt_GU.png')
+		self.bottom_block_image = load_image(r'Map/Dirt.png')
 		self.display_place = pygame.display.get_surface()
 		self.background = load_image('background.png')
 		self.back_rect = self.background.get_rect(topleft=(-200, 0))
 		self.map_matrix = self.generate(int(map_width // 20), int(map_height // 20), int(map_height // 50), 3)
 		self.rect_list = [pygame.Rect(x * 50, y * 50, 50, 50) for y in range(len(self.map_matrix))
-		                  for x in range(len(self.map_matrix[y])) if self.map_matrix[y][x] != ' ']
+						  for x in range(len(self.map_matrix[y])) if self.map_matrix[y][x] != ' ']
 		self.pos_x = 0
 		self.pos_y = 0
 		self.vertical_speed, self.horisontal_speed = 0, 0
@@ -29,7 +32,7 @@ class Map(SpriteGroup):
 			case 'down':
 				self.down_pressed = state
 
-	def collides(self, side):
+	def hero_collides(self, side):
 		match side:
 			case 'top':
 				rects = game.hero.rect.collidelistall(self.rect_list)
@@ -70,8 +73,8 @@ class Map(SpriteGroup):
 		return 0
 
 	def acceleration(self):
-		left, right = self.collides('left'), self.collides('right')
-		up, bottom = self.collides('top'), self.collides('bottom')
+		left, right = self.hero_collides('left'), self.hero_collides('right')
+		up, bottom = self.hero_collides('top'), self.hero_collides('bottom')
 		if left or right:
 			self.horisontal_speed = 0
 		else:
@@ -93,30 +96,28 @@ class Map(SpriteGroup):
 			else:
 				self.vertical_speed = 0
 		else:
-			self.vertical_speed += 0.25 if self.vertical_speed < 5 else 0
+			self.vertical_speed += 0.25 if self.vertical_speed < 4 else 0
 
 		self.pos_x += self.horisontal_speed
 		self.pos_y += self.vertical_speed
 
-	def custom_draw(self):
-		self.acceleration()
-		self.display_place.blit(self.background, ((game.game_time / 3) % self.background.get_width(), -200))
-		self.display_place.blit(self.background, (((game.game_time / 3) % self.background.get_width())
-		                                          - self.background.get_width(), -200))
-		self.place()
-
 	def place(self):
-		top_image = load_image(r'Map/Dirt_GU.png')
-		bot_image = load_image(r'Map/Dirt.png')
 		self.rect_list = []
 		for y in range(len(self.map_matrix)):
 			for x in range(len(self.map_matrix[y])):
 				if self.map_matrix[y][x] != ' ' and self.map_matrix[y][x] != 'P':
 					self.rect_list.append(pygame.Rect(x * 50 - self.pos_x, y * 50 - self.pos_y, 50, 50))
 					if self.map_matrix[y][x] == '-':
-						screen.blit(top_image, self.rect_list[-1])
+						screen.blit(self.top_block_image, self.rect_list[-1])
 					elif self.map_matrix[y][x] == '█':
-						screen.blit(bot_image, self.rect_list[-1])
+						screen.blit(self.bottom_block_image, self.rect_list[-1])
+
+	def custom_draw(self):
+		self.acceleration()
+		self.display_place.blit(self.background, ((game.game_time / 3) % self.background.get_width(), -200))
+		self.display_place.blit(self.background, (((game.game_time / 3) % self.background.get_width())
+												  - self.background.get_width(), -200))
+		self.place()
 
 	def generate(self, x, y, earth_level, max_level):
 		sp = []
@@ -164,7 +165,7 @@ class Hero(Sprite):
 		self.rect = pygame.Rect((self.pos_x + 12, self.pos_y + 15), (32, 76))
 		self.wrect = pygame.Rect((self.pos_x + 8, self.pos_y + 24), (40, 65))
 		game.hero_group.update()
-		if not game.game_map.collides('bottom'):
+		if not game.game_map.hero_collides('bottom'):
 			if game.game_map.horisontal_speed > 0 or self.last_side == 'right':
 				screen.blit(self.image_jump.image_frame(), (self.pos_x, self.pos_y - 5))
 			elif game.game_map.horisontal_speed < 0 or self.last_side == 'left':
@@ -191,36 +192,58 @@ class Hero(Sprite):
 		elif game.game_map.horisontal_speed < 0:
 			self.last_side = 'left'
 
+class Enemies:
+	def __init__(self):
+		self.display_place = pygame.display.get_surface()
+		self.enemy_test = load_image(r'hitbox.png')
+		self.enemies = []
+	def add_enemy(self, x, y):
+		self.enemies.append(pygame.Rect(x, y))
+
+	def draw(self):
+		for i in range(self.enemies):
+			self.display_place.blit()
+
 
 class OverlayingGUI(Sprite):
 	def __init__(self, group):
 		self.display_place = pygame.display.get_surface()
 		super(OverlayingGUI, self).__init__(group)
+		self.menu_r = None
 		self.score = None
 		self.health = None
 		self.exit_r = None
-		self.settings_r = None
 		self.continue_r = None
 		self.start_r = None
 		self.font_50 = pygame.font.Font('Data/m5x7.ttf', 50)
 		self.font_100 = pygame.font.Font('Data/m5x7.ttf', 100)
 		self.font_150 = pygame.font.Font('Data/m5x7.ttf', 150)
 		self.pause_trans_gray = load_image(r'gray_t.png')
+		self.old_score = data['Best_score']
 
 	def update_values(self):
-		self.score = self.font_50.render(f'score: {((game.game_time // 60) * 7) + (game.hero.collected_coins * 10)}',
-		                                 True, (255, 255, 255))
+		self.score_points = ((game.game_time // 60) * 7) + (game.hero.collected_coins * 10) + self.old_score
+		game.data['Best_score'] = self.score_points
+		self.score = self.font_50.render(f'score: {self.score_points}', True, (255, 255, 255))
 		self.health = self.font_100.render(f'HP: {game.hero.health}', True, (100, 255, 100))
 		self.display_place.blit(self.score, ((self.display_place.get_width()) - self.score.get_width(),
-		                                     self.display_place.get_height() - self.score.get_height()))
+											 self.display_place.get_height() - self.score.get_height()))
 		self.display_place.blit(self.health, (self.health.get_width() - 220, self.display_place.get_height()
-		                                      - self.health.get_height()))
+											  - self.health.get_height()))
 
 	def pause_overlay(self):
 		text = self.font_100.render("Game Paused", True, (255, 255, 255))
 		self.display_place.blit(self.pause_trans_gray, (0, 0))
 		self.display_place.blit(text,
-		                        (text.get_width() - 350, screen.get_height() // 9))
+								(text.get_width() - 350, screen.get_height() // 9))
+
+		continue_t = self.font_100.render('Continue', True, (255, 255, 255))
+		self.continue_r = pygame.Rect((150, 300), (continue_t.get_width(), continue_t.get_height()))
+		self.display_place.blit(continue_t, (150, 300))
+
+		menu_t = self.font_100.render('Main menu', True, (255, 255, 255))
+		self.menu_r = pygame.Rect((150, 400), (menu_t.get_width(), menu_t.get_height()))
+		self.display_place.blit(menu_t, (150, 400))
 
 	def menu_overlay(self):
 		name = self.font_150.render('Game title', True, (255, 255, 255))
@@ -236,29 +259,40 @@ class OverlayingGUI(Sprite):
 		self.continue_r = pygame.Rect((150, 400), (continue_t.get_width(), continue_t.get_height()))
 		self.display_place.blit(continue_t, (150, 400))
 
-		settings_t = self.font_100.render('Settings', True, (255, 255, 255))
-		self.settings_r = pygame.Rect((150, 500), (settings_t.get_width(), settings_t.get_height()))
-		self.display_place.blit(settings_t, (150, 500))
-
 		exit_t = self.font_100.render('Quit', True, (255, 255, 255))
-		self.exit_r = pygame.Rect((150, 600), (exit_t.get_width(), exit_t.get_height()))
-		self.display_place.blit(exit_t, (150, 600))
+		self.exit_r = pygame.Rect((150, 500), (exit_t.get_width(), exit_t.get_height()))
+		self.display_place.blit(exit_t, (150, 500))
 
 
 def terminate():
+	with open('Data/Data.json', 'w') as datafile:
+		json.dump(game.data, datafile)
 	pygame.quit()
 	sys.exit()
 
 
 class MainGame:
-	def __init__(self):
+	def __init__(self, database):
+		self.paused = 2  # 0-game, 1-pause, 2-menu
+		self.data = database
+		self.overlay = None
+		self.overlay_group = None
+		self.game_time = None
+		self.hero = None
+		self.hero_group = None
+		self.map_group = None
+		self.game_map = None
+		self.game_map = None
+		self.display_place = None
+		self.new_game()
+
+	def new_game(self):
 		self.display_place = pygame.display.get_surface()
 		self.game_map = Map(width, height)
 		self.map_group = SpriteGroup()
 		self.hero_group = SpriteGroup()
 		self.hero = Hero(*self.game_map.spawn_point, self.hero_group)
 		self.game_time = 0
-		self.paused = 2  # 0-game, 1-pause, 2-menu
 		self.overlay_group = SpriteGroup()
 		self.overlay = OverlayingGUI(self.overlay_group)
 
@@ -306,6 +340,14 @@ class MainGame:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					terminate()
+				elif event.type == pygame.MOUSEBUTTONDOWN:
+					x, y = pygame.mouse.get_pos()
+					if self.overlay.continue_r.left < x < self.overlay.continue_r.right and \
+							self.overlay.continue_r.top < y < self.overlay.continue_r.bottom:
+						self.paused = 0
+					elif self.overlay.menu_r.left < x < self.overlay.menu_r.right and \
+							self.overlay.menu_r.top < y < self.overlay.menu_r.bottom:
+						self.paused = 2
 				elif event.type == pygame.KEYDOWN:
 					if event.key == pygame.K_ESCAPE:
 						self.paused = 0
@@ -332,8 +374,16 @@ class MainGame:
 				elif event.type == pygame.MOUSEBUTTONDOWN:
 					x, y = pygame.mouse.get_pos()
 					if self.overlay.start_r.left < x < self.overlay.start_r.right and \
-						self.overlay.start_r.top < y < self.overlay.start_r.bottom:
+							self.overlay.start_r.top < y < self.overlay.start_r.bottom:
+						game.new_game()
+						game.overlay.old_score = 0
 						self.paused = 0
+					elif self.overlay.continue_r.left < x < self.overlay.continue_r.right and \
+							self.overlay.continue_r.top < y < self.overlay.continue_r.bottom:
+						self.paused = 0
+					elif self.overlay.exit_r.left < x < self.overlay.exit_r.right and \
+							self.overlay.exit_r.top < y < self.overlay.exit_r.bottom:
+						terminate()
 			screen.blit(background, ((time / 3) % background.get_width(), -200))
 			screen.blit(background, (((time / 3) % background.get_width()) - background.get_width(), -200))
 			self.overlay.menu_overlay()
@@ -352,11 +402,13 @@ class MainGame:
 
 
 if __name__ == '__main__':
+	with open('Data/Data.json') as data_file:
+		data = json.load(data_file)
 	clock = pygame.time.Clock()
 	pygame.init()
-	size = width, height = 1600, 800
+	size = width, height = data['resolution']
 	screen = pygame.display.set_mode(size)
 
-	game = MainGame()
+	game = MainGame(data)
 	while True:
 		game.opener(game.paused)
